ba6901faea324e4ad13d77a176aec498df9c1471
Amarnath Reddy <tamarnathr@gmail.com>
2025-05-14 07:50:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+function secondHighest(arr) {
+    if (arr.length < 2) return -Infinity;
+
+    let max = -Infinity;
+    let second = -Infinity;
+
+    for (let num of arr) {
+        if (num > max) {
+            second = max;
+            max = num;
+        } else if (num < max && num > second) {
+            second = num;
+        }
+    }
+
+    return second === -Infinity ? -Infinity : second;
+}
+
